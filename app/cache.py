946bf058f13285ecaef91e7a778551b7a7babb07
sakshamchitkara-dotcom <275204628+sakshamchitkara-dotcom@users.noreply.github.com>
2026-09-25 01:24:38 -0700

    chore: add a TODO, a pyproject with ranges and CODEOWNERS for autopilot v0.2 checks

--- app/cache.py
+++ app/cache.py
@@ -8,6 +8,11 @@ def get(key):
     return _CACHE.get(key)
 
 
+def clear():
+    # TODO: expose cache hit/miss counters
+    _CACHE.clear()
+
+
 def put(key, value):
     # FIXME: not thread safe
     _CACHE[key] = (time.time(), value)
